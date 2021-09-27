cb94f448d2a700a07ab23c93198ba4b857df0306
David Hörl <hoerldavid@gmail.com>
2021-09-27 17:56:24 +0200

    create outline ROIs, show parent in ROI name, remove old code

--- src/main/java/yeastmate/YeastMate.java
+++ src/main/java/yeastmate/YeastMate.java
@@ -5,8 +5,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.stream.StreamSupport;
 
 import org.apache.commons.math3.stat.descriptive.rank.Percentile;
@@ -27,11 +29,14 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 import ij.ImagePlus;
+import ij.gui.PolygonRoi;
 import ij.gui.Roi;
+import ij.gui.Wand;
 import ij.io.FileInfo;
 import ij.io.Opener;
 import ij.io.TiffEncoder;
 import ij.plugin.frame.RoiManager;
+import ij.process.ImageProcessor;
 import ij.process.LUT;
 import net.imagej.ImageJ;
 import net.imagej.lut.LUTService;
@@ -39,6 +44,7 @@ import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.display.ColorTable;
+import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.RealType;
@@ -66,25 +72,25 @@ public class YeastMate implements Command, Previewable {
 	private Double scoreThresholdSingle = 0.9;
 
 	@Parameter(label = "Detection score threshold (matings)", style = "slider", min = "0", max = "1", stepSize = "0.01")
-	private Double scoreThresholdMating = 0.5;
+	private Double scoreThresholdMating = 0.75;
 
 	@Parameter(label = "Detection score threshold (buddings)", style = "slider", min = "0", max = "1", stepSize = "0.01")
-	private Double scoreThresholdBudding = 0.5;
+	private Double scoreThresholdBudding = 0.75;
 
 	@Parameter(label = "Minimum Intensity Quantile for Normalization", style = "slider", min = "0.005", max = "1", stepSize = "0.005")
-	private Double minNormalizationQualtile = 0.01;
+	private Double minNormalizationQualtile = 0.015;
 
 	@Parameter(label = "Maximum Intensity Quantile for Normalization", style = "slider", min = "0.005", max = "1", stepSize = "0.005")
-	private Double maxNormalizationQualtile = 0.995;
+	private Double maxNormalizationQualtile = 0.985;
 
-	@Parameter(label = "Add single cell bounding boxes to ROI Manager?")
-	private Boolean addSingleBoxes = false;
+	@Parameter(label = "Add single cell ROIs to ROI Manager?")
+	private Boolean addSingleRois = false;
 
-	@Parameter(label = "Add mating bounding boxes to ROI Manager?")
-	private Boolean addMatingBoxes = true;
+	@Parameter(label = "Add mating ROIs to ROI Manager?")
+	private Boolean addMatingRois = true;
 
-	@Parameter(label = "Add budding bounding boxes to ROI Manager?")
-	private Boolean addBuddingBoxes = false;
+	@Parameter(label = "Add budding ROIs to ROI Manager?")
+	private Boolean addBuddingRois = false;
 
 	@Parameter(label = "Show segmentation mask?")
 	private Boolean showSegmentation = true;
@@ -92,8 +98,12 @@ public class YeastMate implements Command, Previewable {
 	@Parameter(label = "Only include cells from selected classes in mask?")
 	private Boolean onlySelectedClassesInMask = false;
 
+	// add ROIs as outlines or boxes?
+	private static Boolean addOutlineRois = true;
+
 	@Parameter(label = "IP adress of detection server", style = "server-status")
-	private String ipAdress = "127.0.0.1:5000";
+	private String ipAdress = "127.0.0.1:11005";
+
 
 	@Override
 	public void run() {
@@ -107,22 +117,6 @@ public class YeastMate implements Command, Previewable {
 
 		detect();
 	}
-
-//	private BufferedImage base64toBufferedImg(String b64img) {
-//		BufferedImage image = null;
-//		byte[] imageByte;
-//
-//		try {
-//			imageByte = Base64.getDecoder().decode(b64img);
-//			ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
-//			image = ImageIO.read(bis);
-//		}
-//		catch (IOException e) {
-//			log.info(e);
-//		}
-//
-//		return image;
-//	}
 
 	public <T extends RealType<T>> void detect() {
 
@@ -182,29 +176,37 @@ public class YeastMate implements Command, Previewable {
 
 			// get response as JSON
 			CloseableHttpResponse response = HttpClients.createDefault().execute(conn);
-//			JSONTokener tokener = new JSONTokener(response.getEntity().getContent());
 			JSONObject result = new JSONObject(EntityUtils.toString( response.getEntity() ));
 
 			response.close();
 
 			RoiManager manager = RoiManager.getInstance();
-			if (manager == null && (addSingleBoxes || addMatingBoxes || addBuddingBoxes )){
+			if (manager == null && (addSingleRois || addMatingRois || addBuddingRois )){
 				manager = new RoiManager();
 			}
 
-			// NB: does not work because ImageIO can't read tiff (pre java 9)
-//			BufferedImage base64toBufferedImg = base64toBufferedImg( result.getString( "mask" ) );
-//			ImagePlus maaaaa = new ImagePlus("Temporary mask", base64toBufferedImg);
-//			maaaaa.show();
-			
-			ImagePlus mask = null;
-			if (showSegmentation)
+			// mask is returned as base64-encoded 16-bit TIFF
+			ImagePlus mask = new Opener().openTiff( 
+					new ByteArrayInputStream( Base64.getDecoder().decode( result.getString( "mask" ) ) ),
+					"segmentation of " + image.getTitle() 
+					);
+
+			final ImageProcessor maskProcessor = mask.getProcessor();
+			final Wand wand = new Wand( maskProcessor );
+			Map<Integer, Roi> labelsToRois = new HashMap<>();
+
+			final Img< T > maskRAI = ImageJFunctions.wrapReal( mask );
+			final Cursor< T > cur = maskRAI.localizingCursor();
+			while (cur.hasNext())
 			{
-				// mask is returned as base64-encoded 16-bit TIFF
-				mask = new Opener().openTiff( 
-						new ByteArrayInputStream( Base64.getDecoder().decode( result.getString( "mask" ) ) ),
-						"segmentation of " + image.getTitle() 
-						);
+				cur.fwd();
+				final Integer lab = (int) cur.get().getRealFloat();
+				if (! labelsToRois.containsKey( lab ) && lab > 0)
+				{
+					wand.autoOutline( cur.getIntPosition( 0 ), cur.getIntPosition( 1 ), 0.0, Wand.EIGHT_CONNECTED );
+					final Roi roi = new PolygonRoi( wand.xpoints, wand.ypoints, wand.npoints, Roi.FREELINE );
+					labelsToRois.put( lab, roi );
+				}
 			}
 
 			final JSONObject thingsJSON = result.getJSONObject( "detections" );
@@ -218,8 +220,6 @@ public class YeastMate implements Command, Previewable {
 				JSONArray classes = thing.getJSONArray("class");
 				for (int i=0; i<classes.length(); i++)
 				{
-
-//					objectsOverThreshold.add( Integer.parseInt( key ) );
 
 					JSONArray box = thing.getJSONArray("box");
 
@@ -239,27 +239,43 @@ public class YeastMate implements Command, Previewable {
 					else if (objectClassCode.equals( "2" ))
 						objectClass = "budding";
 					else if (objectClassCode.equals( "1.1" ))
-						objectClass = "mating_mother";
+						objectClass = "mother";
 					else if (objectClassCode.equals( "1.2" ))
-						objectClass = "mating_daughter";
+						objectClass = "daughter";
 					else if (objectClassCode.equals( "2.1" ))
-						objectClass = "budding_mother";
+						objectClass = "mother";
 					else if (objectClassCode.equals( "2.2" ))
-						objectClass = "budding_daughter";
+						objectClass = "daughter";
 
-					if ((addSingleBoxes && objectClassCode.startsWith("0")) || (addMatingBoxes && objectClassCode.startsWith("1")) || (addBuddingBoxes && objectClassCode.startsWith("2"))) {
-						Roi boxroi = new Roi(x,y,w,h);
+					if ((addSingleRois && objectClassCode.startsWith("0")) || (addMatingRois && objectClassCode.startsWith("1")) || (addBuddingRois && objectClassCode.startsWith("2"))) {
+
+						Roi roi = null;
+						// for compound objects or if we do not want outlines: get bbox ROI
+						if (objectClassCode.length() == 1 || !addOutlineRois)
+							roi = new Roi(x,y,w,h);
+						else
+							roi = labelsToRois.get( Integer.parseInt( key ) );
+
+						// NB: total length of ROI name should be < 30 chars!
+						// otherwise it will be truncated in label in resultsTable
+						// see ij.plugin.filter.Analyzer
+						// in our current naming scheme we have 9 chars for cell & parent id
+						String roiName = key + ": " + objectClass;
+						// we have subobject of lifecycle transition -> add parent id in ROI name
+						if (objectClassCode.length() > 1)
+						{
+							roiName += ", " + (objectClassCode.charAt( i ) == '1' ? "mating " : "budding ") + thing.getJSONArray("links").getInt( i-1 );
+						}
 
 						cellsOfSelectedClasses.add( Integer.parseInt( key ) );
-						// TODO: add name of parent here?
-						boxroi.setName( key + ": " + objectClass);
-						boxroi.setPosition(image);
-						manager.addRoi(boxroi);
+						roi.setName( roiName );
+						roi.setPosition(image);
+						manager.addRoi(roi);
 					}
 				}
 
 			}
-			
+
 			if (showSegmentation)
 			{
 				// set objects under threshold to zero
@@ -269,7 +285,7 @@ public class YeastMate implements Command, Previewable {
 							v.setZero();
 					});
 
-				// TODO: extract
+				// TODO: extract?
 				if (lutService.findLUTs().containsKey( LABEL_LUT_NAME ))
 				{
 					ColorTable lutColorTable = lutService.loadLUT( lutService.findLUTs().get( LABEL_LUT_NAME ) );
@@ -287,63 +303,9 @@ public class YeastMate implements Command, Previewable {
 					LUT lut = new LUT(reds, greens, blues);
 					mask.setLut( lut );
 				}
-				
-				
-				mask.show();
 
+				mask.show();
 			}
-			
-			
-			
-//			for (int i = 0; i < result.getJSONArray("things").length(); i++) {
-//				JSONObject thing = result.getJSONArray("things").getJSONObject(i);
-//				
-//				result.getJSONObject( "things" );
-//
-//				double score = thing.getDouble("score");
-//
-//				if (score > scoreThreshold) {
-//					JSONArray box = thing.getJSONArray("box");
-//
-//					int x = box.getInt(0);
-//					int y = box.getInt(1);
-//					int w = box.getInt(2) - box.getInt(0);
-//					int h = box.getInt(3) - box.getInt(1);
-//
-//					if (addBoxes == true) {
-//						Roi boxroi = new Roi(x,y,w,h);
-//						boxroi.setPosition(image);
-//						manager.addRoi(boxroi);
-//					}
-//
-//					if (addMasks == true) {
-//						String maskString = thing.getString("mask");
-//						BufferedImage maskI = base64toBufferedImg(maskString);
-//						ImagePlus maskImg = new ImagePlus("Temporary mask", maskI);
-//
-//						ImageProcessor ip = maskImg.getProcessor();
-//						ip.setThreshold(25, 255, ImageProcessor.NO_LUT_UPDATE);
-//
-//						ThresholdToSelection tts = new ThresholdToSelection();
-//						Roi maskroi = tts.run(maskImg);
-//
-//						maskroi.setPosition(image);
-//						maskroi.setLocation(x,y);
-//
-//						manager.addRoi(maskroi);
-//
-//						RandomAccess< ShortType > cur = masks.randomAccess();
-//						Cursor< UnsignedByteType > itMaskI = ImageJFunctions.wrapByte( maskImg ).localizingCursor();
-//						while (itMaskI.hasNext())
-//						{
-//							itMaskI.fwd();
-//							cur.setPosition( itMaskI );
-//							cur.get().setReal( itMaskI.get().get() / 255 * i );
-//						}
-//						maskImg.close();
-//					}
-//				}
-//			}
 
 		}
 		catch(IOException a) {
@@ -363,18 +325,6 @@ public class YeastMate implements Command, Previewable {
 	public void cancel() {
 		log.info("YeastMate: canceled");
 	}
-
-
-
-//	protected void localChanged() {
-//		if (localChoice.equals("Local detection")) {
-//			tmpIpAdress = ipAdress;
-//			ipAdress = "127.0.0.1:5000";
-//		}
-//		else {
-//			ipAdress = tmpIpAdress;
-//		}
-//	}
 
 	public static void main(final String... args) throws Exception {
 		final ImageJ ij = new ImageJ();
