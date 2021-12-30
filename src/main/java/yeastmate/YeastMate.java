314f251bb8f5a478357c38e9fb9f894f3d4426b9
David Hörl <hoerldavid@gmail.com>
2021-12-30 18:04:54 +0100

    WIP on handling timeseries, relabelling for single frame works

--- src/main/java/yeastmate/YeastMate.java
+++ src/main/java/yeastmate/YeastMate.java
@@ -8,10 +8,13 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.StreamSupport;
 
 import org.apache.commons.math3.stat.descriptive.rank.Percentile;
+import org.apache.http.ParseException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -28,6 +31,7 @@ import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
+import hoerlteam.labeltools.LabelToolsTest;
 import ij.ImagePlus;
 import ij.gui.PolygonRoi;
 import ij.gui.Roi;
@@ -55,6 +59,7 @@ import net.imglib2.view.Views;
 	menuPath = "Plugins>YeastMate")
 public class YeastMate implements Command, Previewable {
 	private static final String LABEL_LUT_NAME = "Fire.lut";
+	private static final String BOUNDARY_STRING = "__BOUNDARY__";
 
 	@Parameter
 	private LogService log;
@@ -118,10 +123,74 @@ public class YeastMate implements Command, Previewable {
 		detect();
 	}
 
-	public <T extends RealType<T>> void detect() {
+	private static JSONObject runRemoteDetection(ImagePlus normalizedImage, String ipAdress, double scoreThresholdSingle, double scoreThresholdMating, double scoreThresholdBudding )
+	{
+		ByteArrayOutputStream imageBytes = new ByteArrayOutputStream();
+		ByteArrayOutputStream jsonBytes = new ByteArrayOutputStream();
 
-		statusService.showStatus( 0, 3, "Preparing Request to Backend" );
+		// write parameters as JSON bytes
+		PrintWriter pw = new PrintWriter(jsonBytes);
+		pw.write("{\"0\":"+scoreThresholdSingle+",\"1\":" +scoreThresholdMating+",\"2\":"+scoreThresholdBudding+"}");
+		pw.close();
 
+		// write normalized image as tiff to bytes
+		try {new TiffEncoder( normalizedImage.getFileInfo() ).write( imageBytes );}
+		catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		// build multipart request as BentoML AnnotatedImage input spec
+		MultipartEntityBuilder multipartBuilder = MultipartEntityBuilder.create();
+		multipartBuilder.setContentType(ContentType.MULTIPART_FORM_DATA);
+		multipartBuilder.setBoundary(BOUNDARY_STRING);
+		multipartBuilder.addBinaryBody("image", imageBytes.toByteArray(), ContentType.IMAGE_TIFF, "image.tiff");
+		multipartBuilder.addBinaryBody( "annotations", jsonBytes.toByteArray(), ContentType.APPLICATION_JSON, "annotations.json");
+
+		HttpPost conn = new HttpPost("http://" + ipAdress + "/predict");
+		conn.setEntity(multipartBuilder.build());
+
+		// get response as JSON
+		CloseableHttpResponse response = null;
+		try {
+			response = HttpClients.createDefault().execute(conn);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		JSONObject result;
+		try {
+			result = new JSONObject(EntityUtils.toString( response.getEntity() ));
+		} catch (ParseException | JSONException | IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		try {response.close();}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	private static ImagePlus parseMaskFromResult(JSONObject result, String title)
+	{
+		// mask is returned as base64-encoded 16-bit TIFF
+		ImagePlus mask;
+		try {
+			mask = new Opener().openTiff( 
+					new ByteArrayInputStream( Base64.getDecoder().decode( result.getString( "mask" ) ) ),
+					"segmentation of " + title 
+					);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return mask;
+	}
+
+	private static <T extends RealType<T>> ImagePlus getNormalizedImagePlus(ImagePlus image, double minNormalizationQuantile, double maxNormalizationQualtile) {
 		// get only currently displayed image as imglib2 RAI
 		RandomAccessibleInterval<T> img = ImageJFunctions.wrapReal( image );
 		if (image.getNChannels() > 1)
@@ -136,7 +205,7 @@ public class YeastMate implements Command, Previewable {
 
 		// get quantiles
 		final Percentile percentileCalculator = new Percentile();
-		final double minPerc = minNormalizationQualtile == 0.0 ? image.getProcessor().getMin() : percentileCalculator.evaluate( pixels, minNormalizationQualtile * 100 );
+		final double minPerc = minNormalizationQuantile == 0.0 ? image.getProcessor().getMin() : percentileCalculator.evaluate( pixels, minNormalizationQuantile * 100 );
 		final double maxPerc = percentileCalculator.evaluate( pixels, maxNormalizationQualtile * 100 );
 
 		// make quantile-normalized copy of img
@@ -152,57 +221,36 @@ public class YeastMate implements Command, Previewable {
 			raNormalized.get().set( x );
 		}
 		ImagePlus normalizedIP = ImageJFunctions.wrap( normalizedImg, "normalized " + image.getTitle());
+		return normalizedIP;
+	}
 
+	public <T extends RealType<T>> void detect() {
+	
+		statusService.showStatus( 0, 3, "Preparing Request to Backend" );
+
+		
+		ImagePlus normalizedIP = getNormalizedImagePlus(image, minNormalizationQualtile, maxNormalizationQualtile);
+	
 		try{
-
-			ByteArrayOutputStream imageBytes = new ByteArrayOutputStream();
-			ByteArrayOutputStream jsonBytes = new ByteArrayOutputStream();
-
-			// write parameters as JSON bytes
-			PrintWriter pw = new PrintWriter(jsonBytes);
-			pw.write("{\"0\":"+scoreThresholdSingle+",\"1\":" +scoreThresholdMating+",\"2\":"+scoreThresholdBudding+"}");
-			pw.close();
-
-			// write normalized image as tiff to bytes
-			new TiffEncoder( normalizedIP.getFileInfo() ).write( imageBytes );
-
-			// build multipart request as BentoML AnnotatedImage input spec
-			MultipartEntityBuilder multipartBuilder = MultipartEntityBuilder.create();
-			multipartBuilder.setContentType(ContentType.MULTIPART_FORM_DATA);
-			multipartBuilder.setBoundary("TEHBOUNDARY");
-			multipartBuilder.addBinaryBody("image", imageBytes.toByteArray(), ContentType.IMAGE_TIFF, "image.tiff");
-			multipartBuilder.addBinaryBody( "annotations", jsonBytes.toByteArray(), ContentType.APPLICATION_JSON, "annotations.json");
-
-			HttpPost conn = new HttpPost("http://" + ipAdress + "/predict");
-			conn.setEntity(multipartBuilder.build());
-
 			statusService.showStatus( 1, 3, "Getting Results from Backend" );
-
-			// get response as JSON
-			CloseableHttpResponse response = HttpClients.createDefault().execute(conn);
-
-			JSONObject result = new JSONObject(EntityUtils.toString( response.getEntity() ));
-
-			response.close();
-
+			
+			JSONObject result = runRemoteDetection(normalizedIP, ipAdress, scoreThresholdSingle, scoreThresholdMating, scoreThresholdBudding);
+	
+	
 			RoiManager manager = RoiManager.getInstance();
 			if (manager == null && (addSingleRois || addMatingRois || addBuddingRois )){
 				manager = new RoiManager();
 			}
-
+	
 			statusService.showStatus( 2, 3, "Parsing Results" );
+	
+			SingleFrameDetectionResults detectionResults = new SingleFrameDetectionResults(result, null);
 
-			// mask is returned as base64-encoded 16-bit TIFF
-			ImagePlus mask = new Opener().openTiff( 
-					new ByteArrayInputStream( Base64.getDecoder().decode( result.getString( "mask" ) ) ),
-					"segmentation of " + image.getTitle() 
-					);
-
-			final ImageProcessor maskProcessor = mask.getProcessor();
+			final ImageProcessor maskProcessor = detectionResults.mask.getProcessor();
 			final Wand wand = new Wand( maskProcessor );
 			Map<Integer, Roi> labelsToRois = new HashMap<>();
-
-			final Img< T > maskRAI = ImageJFunctions.wrapReal( mask );
+	
+			final Img< T > maskRAI = ImageJFunctions.wrapReal( detectionResults.mask );
 			final Cursor< T > cur = maskRAI.localizingCursor();
 			while (cur.hasNext())
 			{
@@ -215,9 +263,10 @@ public class YeastMate implements Command, Previewable {
 					labelsToRois.put( lab, roi );
 				}
 			}
-
+	
 			final JSONObject thingsJSON = result.getJSONObject( "detections" );
-
+	
+			
 			final HashSet< Integer > cellsOfSelectedClasses = new HashSet<>();
 			Iterator<?> keysIt = thingsJSON.keys();
 			while (keysIt.hasNext())
@@ -227,17 +276,17 @@ public class YeastMate implements Command, Previewable {
 				JSONArray classes = thing.getJSONArray("class");
 				for (int i=0; i<classes.length(); i++)
 				{
-
+	
 					JSONArray box = thing.getJSONArray("box");
-
+	
 					int x = box.getInt(0);
 					int y = box.getInt(1);
 					int w = box.getInt(2) - box.getInt(0);
 					int h = box.getInt(3) - box.getInt(1);
-
+	
 					String objectClassCode = classes.getString(i);
 					String objectClass = "";
-
+	
 					// TODO: extract
 					if (objectClassCode.equals( "0" ))
 						objectClass = "single_cell";
@@ -253,50 +302,53 @@ public class YeastMate implements Command, Previewable {
 						objectClass = "mother";
 					else if (objectClassCode.equals( "2.2" ))
 						objectClass = "daughter";
-
+	
 					if ((addSingleRois && objectClassCode.startsWith("0")) || (addMatingRois && objectClassCode.startsWith("1")) || (addBuddingRois && objectClassCode.startsWith("2"))) {
-
+	
 						Roi roi = null;
 						// for compound objects or if we do not want outlines: get bbox ROI
-						if (objectClassCode.length() == 1 || !addOutlineRois)
+						if ((objectClassCode.length() == 1) && !objectClassCode.equals( "0" ) || !addOutlineRois)
 							roi = new Roi(x,y,w,h);
 						else
 							roi = labelsToRois.get( Integer.parseInt( key ) );
-
+	
 						// NB: total length of ROI name should be < 30 chars!
 						// otherwise it will be truncated in label in resultsTable
 						// see ij.plugin.filter.Analyzer
 						// in our current naming scheme we have 9 chars for cell & parent id
-						String roiName = key + ": " + objectClass;
+						String roiName = detectionResults.allLabelRemap.get( Integer.parseInt( key )) + ": " + objectClass;
 						// we have subobject of lifecycle transition -> add parent id in ROI name
 						if (objectClassCode.length() > 1)
 						{
-							roiName += ", " + (objectClassCode.charAt( 0 ) == '1' ? "mating " : "budding ") + thing.getJSONArray("links").getInt( i-1 );
+							roiName += ", " + (objectClassCode.charAt( 0 ) == '1' ? "mating " : "budding ") + detectionResults.allLabelRemap.get(thing.getJSONArray("links").getInt( i-1 ));
 						}
-
+	
 						cellsOfSelectedClasses.add( Integer.parseInt( key ) );
 						roi.setName( roiName );
 						roi.setPosition(image);
 						manager.addRoi(roi);
 					}
 				}
-
+	
 			}
 
 			if (showSegmentation)
 			{
 				// set objects under threshold to zero
 				if (onlySelectedClassesInMask)
-					ImageJFunctions.wrapReal( mask ).forEach( v -> {
+					ImageJFunctions.wrapReal(detectionResults.mask ).forEach( v -> {
 						if (!cellsOfSelectedClasses.contains( (int)(v.getRealFloat())))
 							v.setZero();
 					});
-
+				
+				// relabel mask
+				LabelToolsTest.relabelMap(ImageJFunctions.wrapShort(detectionResults.mask), detectionResults.allLabelRemap);
+	
 				// TODO: extract?
 				if (lutService.findLUTs().containsKey( LABEL_LUT_NAME ))
 				{
 					ColorTable lutColorTable = lutService.loadLUT( lutService.findLUTs().get( LABEL_LUT_NAME ) );
-
+	
 					byte[] reds = new byte[256];
 					byte[] greens = new byte[256];
 					byte[] blues = new byte[256];
@@ -306,14 +358,14 @@ public class YeastMate implements Command, Previewable {
 						greens[i] = (byte) lutColorTable.getResampled( 1, 256, i );
 						blues[i] = (byte) lutColorTable.getResampled( 2, 256, i );
 					}
-
+	
 					LUT lut = new LUT(reds, greens, blues);
-					mask.setLut( lut );
+					detectionResults.mask.setLut( lut );
 				}
-
-				mask.show();
+	
+				detectionResults.mask.show();
 			}
-
+	
 		}
 		catch(IOException a) {
 			log.info(a);
@@ -321,8 +373,94 @@ public class YeastMate implements Command, Previewable {
 		catch(JSONException c) {
 			log.info(c);
 		}
-
+	
 		statusService.showStatus( 3, 3, "YeastMate: Done" );
+	}
+	
+	private class SingleFrameDetectionResults
+	{
+		public LinkedHashSet<Integer> singleLabels;
+		public LinkedHashSet<Integer> matingLabels;
+		public LinkedHashSet<Integer> buddingLabels;
+		public Map<Integer, Integer> singleLabelRemap;
+		public Map<Integer, Integer> compoundLabelRemap;
+		public Map<Integer, Integer> allLabelRemap;
+		public ImagePlus mask;
+		public JSONObject detections;
+		public Map<Integer, double[]> compoundBoxes;
+		public int maxLabel;
+
+		public SingleFrameDetectionResults(JSONObject results, SingleFrameDetectionResults last) {
+			
+			// FIXME: proper name for mask
+			mask = parseMaskFromResult(results, "mask");
+			
+			// FIXME: get max label from old frame
+			int startValue = last == null ? 0 : last.maxLabel;
+			
+			singleLabels = LabelToolsTest.getLabelSet(ImageJFunctions.wrapShort(mask));
+			singleLabelRemap = new HashMap<>();
+			AtomicInteger idx = new AtomicInteger(startValue);
+			for (Integer s: singleLabels) singleLabelRemap.put(s, idx.incrementAndGet());
+
+			compoundBoxes = new HashMap<>();
+			compoundLabelRemap = new HashMap<>();
+			matingLabels = new LinkedHashSet<>();
+			buddingLabels = new LinkedHashSet<>();
+
+			try
+			{
+				detections = results.getJSONObject( "detections" );
+
+				Iterator<?> keysIt = detections.keys();
+				while (keysIt.hasNext())
+				{
+					String key = (String) keysIt.next();
+					JSONObject detection = detections.getJSONObject( key );
+					JSONArray classes = detection.getJSONArray("class");
+
+					// get bounding box of object
+					JSONArray box = detection.getJSONArray("box");
+					int x = box.getInt(0);
+					int y = box.getInt(1);
+					int w = box.getInt(2) - box.getInt(0);
+					int h = box.getInt(3) - box.getInt(1);
+
+					// NB: compound objects have only one class, so we only look at index 0
+					String objectClassCode = classes.getString(0);
+
+					// save label if we have compound object
+					if (objectClassCode.equals( "1" ))
+						matingLabels.add(Integer.parseInt(key));
+					if (objectClassCode.equals( "2" ))
+						buddingLabels.add(Integer.parseInt(key));
+
+					// save box if we have compound object
+					if (objectClassCode.equals( "1" ) || objectClassCode.equals( "2" ))
+						compoundBoxes.put(Integer.parseInt(key), new double[] {x, y, x+w, y+h});
+
+				}
+			}
+			catch (JSONException e)
+			{
+				e.printStackTrace();
+			}
+
+			// map sequential for compound labels as well
+			for (Integer s: matingLabels) compoundLabelRemap.put(s, idx.incrementAndGet());
+			for (Integer s: buddingLabels) compoundLabelRemap.put(s, idx.incrementAndGet());
+
+			// TODO: match labels from last
+
+			allLabelRemap = new HashMap<>();
+			allLabelRemap.putAll(singleLabelRemap);
+			allLabelRemap.putAll(compoundLabelRemap);
+
+			// get maximum (remapped) label
+			AtomicInteger tmpMaxLabel = new AtomicInteger(-1);
+			allLabelRemap.values().forEach(s -> {tmpMaxLabel.set( Math.max(s, tmpMaxLabel.get()));});
+			maxLabel = Math.max(last == null ? 0 : last.maxLabel, tmpMaxLabel.get());
+		}
 	}
 
 	@Override
